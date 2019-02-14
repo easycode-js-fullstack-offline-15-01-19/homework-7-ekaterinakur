d6a77bf6a5329d3954d78ce4d7c345638066c5ab
Ekaterina Kurochka <ekaterina1.kur@gmail.com>
2019-02-15 01:10:53 +0200

    Destructuring: 2 tasks; Arrow functions: 1 task

--- homework.js
+++ homework.js
@@ -1,2 +1,49 @@
-// -------------------------- Home work --------------------------
-// -------------------------- ФИО --------------------------
+// -------------------------- Home work 7 --------------------------
+// -------------------------- Курочка Екатерина --------------------------
+
+//  1. Используя rest оператор и деструктуризацию, создать функцию, которая принимает любое количество аргументов и возвращает объект, содержащий первый аргумент и массив из остатка:
+
+/**
+   * @autor Ekaterina Kurochka
+   * @description для создания объекта, содержащего первый аргумент функции и массив из остатка входящих аргументов ф-ции
+   * @returns {object} объект   
+   */
+function getObj() {
+	const [first, ...other] = arguments;
+	return {first: first, other: other};
+}
+
+getObj('a', 'b', 'c', 'e', 'd');
+
+// //  2. Организовать функцию getInfo, которая принимает объект и выводит в консоль имя (если имени нет, показывать ‘Unknown’) и первые две компании из массива partners
+
+const organisation = {  
+  name: 'Google',   
+  info: { employees: ['Vlad', 'Olga'], partners: ['Microsoft', 'Facebook', 'Xing'] } 
+};
+
+/**
+   * @autor Ekaterina Kurochka
+   * @description Для получения (в консоль) значения поля "имя" (если имени нет - unknown) и первых двух партнеров из массива
+   * @param {object} объект вида { name: ...,  info: { employees: [...], partners: [ … ]  } }
+   * @returns {undefined}   
+   */
+function getInfo(myObj) {
+	let { name = 'Unknown',  info: { partners: [first, second] } } = myObj;
+	// console.log( `Name: ${name}; Partners: ${ [ first, second ] }` );
+	console.log(`Name: ${name}`);
+	console.log(`Partners: ${ [first, second] }`);
+}
+
+getInfo(organisation);
+
+//  3. Переделать функцию с использованием функции-стрелки (в методе reduce тоже использовать arrow function)
+
+const sum = (...props) => {
+	const params = Array.prototype.slice.call(props);
+	if (!params.length) return 0;
+  	return params.reduce((prev, next) => prev + next);
+}
+
+sum(1, 2, 3, 4); // 10
+sum(); // 0
